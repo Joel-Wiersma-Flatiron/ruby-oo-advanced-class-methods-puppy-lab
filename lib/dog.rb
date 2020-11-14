3565c44924cae08f6570c336c7280fb5b9d67af6
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-14 14:38:36 -0700

    commit tree f94ec7799c9d0c7b38adf8193f9fcd2674037b71

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each{|name| puts name.name}
+        #i = 0
+        #while i < @@all.length
+        #    puts @@all[i].instance_variable_get(:@name)
+        #    i += 1
+        #end
+    end
+
+    def save
+        @@all << self
+    end
+end
